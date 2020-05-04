8d3c912d8fc4e57112714055840992bb0111d034
*md <*asiful_haque4@yahoo.com*>
2020-05-04 17:21:03 -0400

    comit

---
+++ Zoopla_TeamOne/src/test/java/com/usa/Myrunner/Zooplarunner.java
@@ -0,0 +1,18 @@
+package com.usa.Myrunner;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(
+		features= {"src/main/resources/FindProperty.feature"},
+		glue = {"com.usa.stepdefination"},
+	    plugin = {"pretty", "html:target/cucumber-reports/cucumber.json",
+		"json:target/cucumber.json"},
+	    dryRun = false,
+		monochrome  = false,
+		strict = false
+		)
+public class Zooplarunner extends AbstractTestNGCucumberTests {
+
+}
+ 
